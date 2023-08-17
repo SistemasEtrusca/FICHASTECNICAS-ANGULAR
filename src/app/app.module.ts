9d7f63b0b06a6201adaaff2ad5005a25c4551379
desarrolladoraFront <daniela.andrade@cafeetrusca.com>
2023-08-17 13:47:08 -0600

    Elimina los componentes innecesarios y modifica el menú principal quitando el botón de buscar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { DescriptionComponent } from './components/description/description.component';
-import { ImagenProductoComponent } from './components/imagen-producto/imagen-producto.component';
-import { IngredientsComponent } from './components/ingredients/ingredients.component';
-import { TableCharacteristicsComponent } from './components/table-characteristics/table-characteristics.component';
-import { CodebarComponent } from './components/codebar/codebar.component';
-import { CodeQRComponent } from './components/code-qr/code-qr.component';
 import { LayoutAccesoriosComponent } from './layout/layout-accesorios/layout-accesorios.component';
 import { LayoutMaquinariaComponent } from './layout/layout-maquinaria/layout-maquinaria.component';
 import { DataService } from './services/data.service';
@@ -22,14 +14,6 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    DescriptionComponent,
-    ImagenProductoComponent,
-    IngredientsComponent,
-    TableCharacteristicsComponent,
-    CodebarComponent,
-    CodeQRComponent,
     LayoutAccesoriosComponent,
     LayoutMaquinariaComponent,
     menuMaquinariaComponent,
